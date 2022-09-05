dfbbfbd46049171893f8088d5b6671b3e3028f3c
shubhom <shbhomsrivastav@gmail.com>
2022-09-05 11:13:17 +0530

    more labcode

--- DSA_lab/q1_dlab4.c
+++ DSA_lab/q1_dlab4.c
@@ -1,17 +1,59 @@
-#include<stdio.h>
+#include <stdio.h>
 
-struct sparse
-{
-    int row,col,val;
+struct employee {
+  struct name {
+    char fname[20];
+    char mname[20];
+    char lname[20];
+
+  } n;
+  struct dob {
+    int dd;
+    int mm;
+    int yy;
+  } d;
+  int ac_no;
+  int balance;
 };
-void readmat(sparse sp[])
-{
-    printf("enter total number number of rows ,column of matrix and total
-    of nonzero values in this\n");             
-    scanf("%d %d %d",&sp[0].row,&sp[0].col,&sp[0].val);
-    printf("now start entering the values by specifying index 
-    position\n");
-    for(int i=1;i<=sp[0].val;i++)
-        scanf("%d %d %d",&sp[i].row,&sp[i].col,&sp[i].val);
+int main() {
+  int n;
+  printf("Enter the number of employee ");
+  scanf("%d", &n);
+  struct employee s[n];
+  for (int i = 0; i < n; i++) {
+    printf("Enter the first ,middle and last name of the employee ");
+    scanf("%s", s[i].n.fname);
+    scanf("%s", s[i].n.mname);
+    scanf("%s", s[i].n.lname);
+    printf("Enter the dob of the employee in dd mm yy ");
+    scanf("%d%d%d", &s[i].d.dd, &s[i].d.mm, &s[i].d.yy);
+    printf("Enter the account number :");
+    scanf("%d", &s[i].ac_no);
+    printf("Enter the balance : ");
+    scanf("%d", &s[i].balance);
+  }
+
+  for (int i = 0; i < n; i++) {
+    printf("\n");
+    if (s[i].balance < 200) {
+      printf("Employee with less than 200");
+      printf("Employee Name : %s %s %s\n", s[i].n.fname, s[i].n.mname,
+             s[i].n.lname);
+      printf("DOB : %d/%d/%d\n", s[i].d.dd, s[i].d.mm, s[i].d.yy);
+      printf("A/C NO : %d\n", s[i].ac_no);
+      printf("Balance : %d\n", s[i].balance);
+      printf("\n\n");
+    }
+    if (s[i].balance > 1000) {
+      s[i].balance += 100;
+      printf("Employee with more than 1000");
+      printf("Employee Name : %s %s %s\n", s[i].n.fname, s[i].n.mname,
+             s[i].n.lname);
+      printf("DOB : %d/%d/%d\n", s[i].d.dd, s[i].d.mm, s[i].d.yy);
+      printf("A/C NO : %d\n", s[i].ac_no);
+      printf("Balance : %d\n", s[i].balance);
+      printf("\n\n");
+    }
+  }
+  return 0;
 }
-:
